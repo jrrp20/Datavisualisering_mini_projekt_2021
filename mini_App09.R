2d0db8663acb884d34d0e178d472002079648a46
skingi20 <skingi20@student.aau.dk>
2021-11-30 14:02:37 +0100

    some stuff

--- mini_App09.R
+++ mini_App09.R
@@ -33,8 +33,7 @@ sea$date <- as.Date(sea$date, format = "%Y-%m-%d")
 ui <- fluidRow(
           column(6, selectInput("city", label = "City:", choices = c("New York","Phoenix","Seattle")), selected = "nyc"),
       # Select variable for y-axis
-          column(6,selectInput("y_var", label = "Dependent variable:",choices = c("average_max_temp","record_min_temp", "record_max_temp"),selected = "record_max_temp"),
-    ),
+          
       #mainPanel(
         #fluidRow(
           column(width = 8, class = "well",
